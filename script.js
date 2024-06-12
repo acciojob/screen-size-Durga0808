d005d82be96b9fc6297f3a421d95025cfc25655c
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-06-12 15:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateWindowSize() {
+	const width=window.innerWidth;
+	const height=window.innerHeight;
+	const div=document.getElementById('sizeInfo');
+	div.innerHTML=`<h1>Width: ${width}, Height: ${height}</h1>`;
+}
+
+updateWindowSize();
+window.addEventListener('resize',updateWindowSize);
